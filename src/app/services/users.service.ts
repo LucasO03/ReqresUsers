bf6508e257246da5297f855d8b325cef69f03072
Lucas Santos de Oliveira <lucas.santos003@outlook.com>
2023-04-14 20:36:29 -0300

    Dados estao mostrando no console

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,14 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../models/User.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  url = 'reqres.in/api/users'
+  url = 'https://reqres.in/api/users'
 
   constructor(private http: HttpClient) { }
 
+  create (user: User){
+    return this.http.post(this.url, user);
+  }
 
+  getAll (){
+    return this.http.get(this.url);
+  }
+
+  getOne (id: number){
+    return this.http.get(`${this.url}/${id}`)
+  }
+
+  update (user: User){
+    return this.http.put(`${this.url}/${user.id}`, user)
+  }
+
+  delete (id: number){
+    return this.http.delete(`${this.url}/${id}`)
+  }
 }
